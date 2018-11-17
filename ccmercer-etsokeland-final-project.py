625a1801fe8d7e30622306fd231433f22896b882
Eli Sokeland <elisokeland@yahoo.com>
2018-11-17 14:13:40 -0500

    Inserted Fox, BinaryFoxTree, and Test class.

---
+++ ccmercer-etsokeland-final-project.py
@@ -0,0 +1,35 @@
+import unittest
+
+class Fox():
+	def __init__(self):
+		pass
+
+	def insert(self, fox):
+		pass
+
+	def delete(self, fox):
+		pass
+
+class BinaryFoxTree(object):
+	def __init__(self):
+		pass
+
+	def insert(self, fox):
+		pass
+
+	def delete(self, fox):
+		pass
+
+	def search(self, fox):
+		pass
+
+	def minimum(self):
+		pass
+
+	def maximum(self):
+		pass
+
+class FoxTree_UnitTest(unittest.TestCase):
+
+	def test_insert_fox(self):
+		foxTree = BinaryFoxTree(array = ())
